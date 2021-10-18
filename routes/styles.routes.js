2810f04afe19335a3541271031ae69a69c766dab
Marc Ramos <59534439+gitmarcramos@users.noreply.github.com>
2021-10-18 13:12:56 +0200

    modifications added to run on port 3001

--- routes/styles.routes.js
+++ routes/styles.routes.js
@@ -3,16 +3,14 @@ const router = express.Router();
 
 const StylesModel = require("./../model/Styles.model.js");
 
-router.get("/styles",  async (req, res, next) => {
-    try {
-    const styleDisplay =  await StylesModel.find() 
+router.get("/", async (req, res, next) => {
+  try {
+    const styleDisplay = await StylesModel.find();
     console.log("styles hbs render");
-   res.render("./../views/dashboard/styles.hbs", {styleDisplay})
-    } 
-    catch(error) {
-        console.log('styles.hbs not render', error)
-    }
-   
-} )
+    res.render("dashboard/styles.hbs", { styleDisplay });
+  } catch (error) {
+    console.log("styles.hbs not render", error);
+  }
+});
 
 module.exports = router;
