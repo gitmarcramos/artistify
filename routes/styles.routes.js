1aa8f64f664f49f6b53fb75a7afd7dcd06d94e54
Marc Ramos <59534439+gitmarcramos@users.noreply.github.com>
2021-10-18 15:03:42 +0200

    create in progress

--- routes/styles.routes.js
+++ routes/styles.routes.js
@@ -6,11 +6,42 @@ const StylesModel = require("./../model/Styles.model.js");
 router.get("/", async (req, res, next) => {
   try {
     const styleDisplay = await StylesModel.find();
-    console.log(styleDisplay)
     console.log("styles hbs render");
     res.render("dashboard/styles.hbs", { styleDisplay });
   } catch (error) {
-      next(error);
+    next(error);
+  }
+});
+
+// DELETE
+router.get("/delete/:id", async (req, res, next) => {
+  try {
+    await StylesModel.findByIdAndRemove(req.params.id);
+    console.log(req.params.id);
+
+    res.redirect("/dashboard/styles");
+  } catch (error) {
+    next(error);
+  }
+});
+
+//RENDER THE PAGE FOR THE FORM CREATE
+router.get("/create", (req, res, next) => {
+  try {
+      res.render("dashboard/create.hbs")
+  } catch (error) {
+    next(error);
+  }
+});
+
+// POST A ITEM CREATED IN THE "DASHBOARD/STYLES"
+router.post("/", async (req, res, next) => {
+  try {
+      await StylesModel.create(req.body)
+
+      res.redirect("/dashboard/styles")
+  } catch (error) {
+    next(error);
   }
 });
 
